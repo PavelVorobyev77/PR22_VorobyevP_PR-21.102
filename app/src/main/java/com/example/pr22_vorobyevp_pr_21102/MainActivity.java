f5bee923cda4b05876fb0c0087938d1223f0a9cd
PavelVorobyev77 <pascha27_05@mail.ru>
2023-10-29 19:35:36 +0700

    pr22

---
+++ app/src/main/java/com/example/pr22_vorobyevp_pr_21102/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.pr22_vorobyevp_pr_21102;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private GridView mGrid;
+    private GridAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mGrid = (GridView)findViewById(R.id.field);
+        mGrid.setNumColumns(6);
+        mGrid.setEnabled(true);
+
+        mAdapter = new GridAdapter(this, 6, 6);
+        mGrid.setAdapter(mAdapter);
+
+        mGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id){
+
+                mAdapter.checkOpenCells ();
+                mAdapter.openCell (position);
+
+                if (mAdapter.checkGameOver())
+                    Toast.makeText (getApplicationContext(), "Игра закончена", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void adjustGridView(){
+
+    }
+}
